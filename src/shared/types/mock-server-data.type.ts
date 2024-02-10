baccf59f2c2fe78fb5598bde2a441a88b3a7a042
Azamat <kastoev2001@gmail.com>
2024-02-10 22:07:37 +0300

    Добавляет поле facilities в моковых данных.

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type mockServerData = {
+  titles: string[];
+  facilities: string[];
+  description: string[];
+  cities: string[];
+  avatars: string[];
+  images: string[];
+  coordinates: string[][];
+  usersName: string[];
+  email: string[];
+};
