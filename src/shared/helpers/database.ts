3c31d2e2f7941eca9f1768154868941f6576bbb1
Azamat <kastoev2001@gmail.com>
2024-07-28 15:06:26 +0300

    Добавит функцию-хелпер getMongoURI.

---
+++ src/shared/helpers/database.ts
@@ -0,0 +1,9 @@
+export const getMongoURI = (
+  username: string,
+  password: string,
+  host: string,
+  port: string
+  databaseName: string
+): string => {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=admin`;
+};
