4b8d6a8a149f1d24ac481b56514ba12cfa2b7f10
Azamat <kastoev2001@gmail.com>
2024-07-28 15:30:16 +0300

    Исправит ошибку и добавит в функцию-хелпер в файл index.ts.

--- src/shared/helpers/database.ts
+++ src/shared/helpers/database.ts
@@ -2,8 +2,8 @@ export const getMongoURI = (
   username: string,
   password: string,
   host: string,
-  port: string
-  databaseName: string
+  port: string,
+  databaseName: string,
 ): string => {
   return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=admin`;
 };
