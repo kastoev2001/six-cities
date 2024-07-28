e5ca2cd29cf173c19e80fc3e01df99f38d545b30
Azamat <kastoev2001@gmail.com>
2024-07-29 00:27:48 +0300

    Добавит тип User в файл index.ts.

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export * from './offer.type.js';
 export * from './housing-type.const.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
+export * from './user.type.js';
