71d6334b339bc8ff8e7def61a2ecd7f6e2c50966
Azamat <kastoev2001@gmail.com>
2024-07-31 00:31:18 +0300

    Добавит поле password в сущность UserEntity.

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -2,7 +2,6 @@ import {defaultClasses, prop, getModelForClass, modelOptions } from '@typegoose/
 import { User, UserValueType } from '../../types/index.js';
 import { UserType } from '../../types/index.js';
 
-
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
@@ -14,31 +13,47 @@ export interface UserEntity extends defaultClasses.Base {}
 })
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User  {
+  constructor(userData: User) {
+    super();
+    this.email = userData.email;
+    this.username = userData.username;
+    this.avatar = userData.avatar;
+    this.type = userData.type;
+  }
   @prop({
-    uniquie: true,
     require: true,
     default: '',
   })
-  public username!: string;
+  public username: string;
 
   @prop({
     required: true,
-    default: '',
+    uniquie: true,
   })
-  public email!: string;
+  public email: string;
   
   @prop({
     required: true,
     default: '',
   })
-  public avatar!: string;
+  public avatar: string;
 
   @prop({
     type: () =>  String,
     enum: Object.values(UserType)
 
   })
-  public type!: UserValueType;
+  public type: UserValueType;
+
+  @prop({
+    require: true,
+    default: '',
+  })
+  private password!: string;
+
+  public getPassword = (): string => {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
