403ddc05997efa17ef9aea905faabdb5bdd59194
Azamat <kastoev2001@gmail.com>
2024-07-30 19:43:17 +0300

    Добавит тип UserValueType.

--- src/shared/types/user.type.ts
+++ src/shared/types/user.type.ts
@@ -1,8 +1,12 @@
 import { UserType } from './housing-type.const.js';
 
+type user = typeof UserType
+
+export type UserValueType = typeof UserType[keyof typeof UserType]
+
 export type User = {
   username: string;
   email: string;
   avatar: string;
-  type: typeof UserType[keyof typeof UserType]
+  type: UserValueType
 }
