e9e27d61e52b880cb69935b004281604bb87a3e9
Azamat <kastoev2001@gmail.com>
2024-08-01 00:21:56 +0300

    Объяденит контейрены зависимостей.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -2,17 +2,15 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 import { Component } from './shared/types/index.js';
 import { RestApplication } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/config/index.js';
-import { DatabaseClient, MongoseDatabaseClient } from './shared/libs/database-client/index.js';
-
-import { PinoLogger, Logger } from './shared/libs/logger/index.js';
+import { createRestApplicationContainer } from './rest/index.js'
+import { createUserContainer } from './shared/modules/user/index.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.RestConfig).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoseDatabaseClient).inSingletonScope();
+  const container = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+  );
+
 
   const application = container.get<RestApplication>(Component.RestApplication);
 
