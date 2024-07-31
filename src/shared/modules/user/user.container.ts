35bea3e5fb9e3be4f9c0eb74e6b4f5b41cdf9918
Azamat <kastoev2001@gmail.com>
2024-07-31 23:47:32 +0300

    Исправит опечаток в файле user.container.ts.

--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -1,12 +1,12 @@
 import { Container } from 'inversify';
 import { types } from '@typegoose/typegoose';
 import { Component } from '../../types/index.js';
-import { UserModel } from './user.entity.js';
+import { UserEntity, UserModel } from './user.entity.js';
 
 export const createUserContainer = (): Container => {
   const userContainer = new Container();
 
-  userContainer.bind<types.ModelType<UserModel>>(Component.UserModel).toConstantValue(UserModel)
+  userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel)
 
   return userContainer;
 };
