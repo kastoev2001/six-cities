1d961feee3811bc2ae043b9987269f385eefc905
Azamat <kastoev2001@gmail.com>
2024-08-17 12:31:36 +0300

    Добавит логер в внутри класса DefaultOfferService.

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -5,15 +5,19 @@ import { injectable, inject } from 'inversify'
 import { OfferEntity } from './offer.entity.js'
 import { CreateOfferDTO } from './dto/create-offer.dto.js'
 import { ModelType } from '@typegoose/typegoose/lib/types.js'
+import { Logger } from '../../libs/logger/index.js';
 
 injectable()
 export class DefaultOfferService implements OfferService {
   constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.OfferModel) private readonly offerModule: ModelType<OfferEntity>,
   ) {}
 
   public create = async (dto: CreateOfferDTO): Promise<DocumentType<OfferEntity>> => {
-    return this.offerModule.create(dto);
+    const result = await this.offerModule.create(dto);
+    this.logger.info(`New offer created: ${dto.title}`);
+    return result;
   }
 
   public findById = async (id: string): Promise<DocumentType<OfferEntity> | null> => {
