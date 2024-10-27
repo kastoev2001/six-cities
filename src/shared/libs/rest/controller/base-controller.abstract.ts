7d29cd88a5bc4769504848a7975e6a574f09c909
Azamat <kastoev2001@gmail.com>
2024-10-27 16:10:33 +0300

    Добавит абстрактный класс BaseController.

---
+++ src/shared/libs/rest/controller/base-controller.abstract.ts
@@ -0,0 +1,38 @@
+import { Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { injectable } from 'inversify';
+
+import { Logger } from '../../logger/index.js';
+import { Controller } from './controller.interface.js';
+import { Route } from '../types/route.interface.js';
+
+@injectable()
+export abstract class BaseContrller implements Controller {
+  protected readonly _rotuter: Router;
+
+  constructor(
+    protected readonly logger: Logger,
+  ) {
+    this._rotuter = Router();
+  }
+
+  get route() {
+    return this._rotuter;
+  }
+
+  public addRoute = (route: Route) => {
+    this._rotuter[route.method](route.path, route.handler);
+    this.logger.info(`Route registered: ${route.method.toLowerCase()} ${route.path}`);
+  };
+
+  public send = <T>(res: Response, statusCode: number, data: T) => res
+    .type('application/json')
+    .status(statusCode)
+    .json(data);
+
+  public ok = <T>(res: Response, data: T) => this.send(res, StatusCodes.OK, data);
+  
+  public created = <T>(res: Response, data: T) => this.send(res, StatusCodes.CREATED, data);
+  
+  public noContent = <T>(res: Response, data: T) => this.send(res, StatusCodes.NO_CONTENT, data);
+}
---
+++ src/shared/libs/rest/index.ts
@@ -0,0 +1,4 @@
+export * from './types/http-mothod.enum.js';
+export * from './types/route.interface.js';
+export * from './controller/base-controller.abstract.js';
+export * from './controller/controller.interface.js';
