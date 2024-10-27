9a163ab9e36d330692bb5edd226811fbd09e2905
Azamat <kastoev2001@gmail.com>
2024-10-27 13:02:23 +0300

    Добавит интерфейс Controller.

---
+++ src/shared/libs/rest/controller/controller.interface.ts
@@ -0,0 +1,10 @@
+import { Route } from '../types/route.interface.js';
+import { Response } from 'express';
+
+export interface Controller {
+  addRoute: (route: Route) => void;
+  send: <T>(res: Response, statusCode: number, data: T) => void;
+  ok: <T>(res: Response, data: T) => void;
+  created: <T>(res: Response, data: T) => void;
+  noContent: <T>(res: Response, data: T) => void;
+}
