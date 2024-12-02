c4393ef00a1eede54016ddafd974beb4cc94ed6b
Azamat <kastoev2001@gmail.com>
2024-12-02 16:52:54 +0300

    Добавит функцию createObjectError.

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -18,3 +18,9 @@ export const getRandomBoolean = () => Math.random() >= 0.5;
 
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
+
+export const createObjectError = (message: string) => {
+  return {
+    error: message,
+  };
+};
