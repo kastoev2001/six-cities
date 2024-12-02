b0c7125fb1ecb76b8b334018bbc16b4a41028679
Azamat <kastoev2001@gmail.com>
2024-12-02 16:57:28 +0300

    фиксация коммита для пуша.

--- src/shared/libs/rest/exception-filter/app-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -4,6 +4,8 @@ import { Component } from '../../../types/component.enum.js';
 import { Logger } from '../../logger/index.js';
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes'
+import { HttpError } from '../errors/http-error.js';
+import { createObjectError } from '../../../helpers/index.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -13,7 +15,12 @@ export class AppExceptionFilter implements ExceptionFilter {
   ) {
     this.logger.info('Register AppExceptionFilter.');
   }
-  
+
+  private handlerHttpError = (Status) => {
+
+  }
+  private handlerOtherError = () => {}
+
   public catch = (error: Error, _req: Request, res: Response, _next: NextFunction): void => {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
